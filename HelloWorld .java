169e306408729589857b2eb320b16e46bb3b592f
HARSHAD R <harshadambi20@gmail.com>
2025-09-24 22:36:09 +0530

    Add files via upload

---
+++ HelloWorld .java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+		System.out.println("Hello world");
+          }
+}
